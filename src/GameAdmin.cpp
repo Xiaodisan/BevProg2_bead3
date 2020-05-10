399972202f61c88d3cfaf40dfc9432496c21bf26
Xiaodisan <dr4kuma@gmail.com>
2020-05-10 22:36:30 +0200

    (rossz indexek - javítva)

--- src/GameAdmin.cpp
+++ src/GameAdmin.cpp
@@ -18,10 +18,14 @@ int GameAdmin::evaluate() {
     int counter = 1;
     if(nX && nY) if((*nX) != pX || (*nY) != pY) {
         pX = *nX; pY = *nY;
+        std::cout << pX << ':' << pY << std::endl << (*(*table)[pY][pX]) << std::endl << std::endl;
         steps++;
         counter = std::max(std::max(countInDirection(1,1),countInDirection(0,1)),std::max(countInDirection(1,-1),countInDirection(1,0)));
 //        std::cout << (*table)[pY][pX] << ':' << *(*table)[pY][pX] << "\n";
-        if(counter >= goal) return (*(*table)[*nY][*nY]);
+        if(counter >= goal)
+        {
+            return (*(*table)[*nY][*nX]);
+        }
     }
     if(steps >= (*table).size()*(*table)[0].size()) return 629;
     return 0;
