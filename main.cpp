9571f448ae06c54fb1eb63c626b3e266e45aa261
Xiaodisan <dr4kuma@gmail.com>
2020-05-10 20:18:11 +0200

    main-ből minden grafikus megjelítés a game_loop-ba helyezve

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+
 #include "master.h"
 
 using namespace genv;
 
-int game_loop(event ev) {
+int game_loop() {
+    gout.open(XX,YY);
+    event ev;
+    gin.timer(40);
+    gout << move_to(0,0) << color(108, 187, 164) << box(XX,YY) << color(255,255,255);
     Amoeba_field jatek(10,10,XX-20,YY-20,11);
     while(gin >> ev && ev.keycode != key_escape) {
         if(ev.type == ev_timer) {
@@ -19,11 +24,7 @@ int game_loop(event ev) {
 
 int main()
 {
-    gout.open(XX,YY);
-    event ev;
-    gin.timer(40);
-    gout << move_to(0,0) << color(108, 187, 164) << box(XX,YY) << color(255,255,255);
-    int result = game_loop(ev);
+    int result = game_loop();
     std::cout << result;
     return 0;
 }
