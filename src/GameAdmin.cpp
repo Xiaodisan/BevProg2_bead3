83cfae51a5bf17fdb69757e3f6508270f39d842e
Xiaodisan <dr4kuma@gmail.com>
2020-05-10 22:31:40 +0200

    GameAdmin ellenörző tagfüggvény javítása

--- src/GameAdmin.cpp
+++ src/GameAdmin.cpp
@@ -19,7 +19,9 @@ int GameAdmin::evaluate() {
     if(nX && nY) if((*nX) != pX || (*nY) != pY) {
         pX = *nX; pY = *nY;
         steps++;
+        counter = std::max(std::max(countInDirection(1,1),countInDirection(0,1)),std::max(countInDirection(1,-1),countInDirection(1,0)));
 //        std::cout << (*table)[pY][pX] << ':' << *(*table)[pY][pX] << "\n";
+        if(counter >= goal) return (*(*table)[*nY][*nY]);
     }
     if(steps >= (*table).size()*(*table)[0].size()) return 629;
     return 0;
@@ -27,13 +29,15 @@ int GameAdmin::evaluate() {
 
 int GameAdmin::countInDirection(int kx, int ky) { ///irányvektorokkal megadva az irányt
     int counter = 1;
-    int playerID = *(*table)[*nY][*nX];
-    for(int i = 1; i < goal && (*nY)+i*kx < (*table).size() && (*nX)+i*ky < (*table)[*nY].size() ;i++) {
-        if(*(*table)[(*nY)+i*ky][(*nX)+i*kx] != playerID) break;
+    int playerID = *((*table)[pY][pX]);
+    for(int i = 1; i < goal && pX+i*kx < (*table).size() && pY+i*ky < (*table)[pY].size() && pX+i*kx >= 0 && pY+i*ky >= 0;i++) {
+        int newID = *(*table)[pY+i*ky][pX+i*kx];
+        if(newID != playerID) break;
         counter++;
     }
-    for(int i = 1; i < goal && (*nY)-i*kx >= 0 && (*nX)-i*ky > 0; i++) {
-        if(*(*table)[(*nY)+i*ky][(*nX)+i*kx] != playerID) break;
+    for(int i = 1; i < goal && pX-i*kx >= 0 && pY-i*ky >= 0 && pX-i*kx < (*table).size() && pY-i*ky  < (*table)[pY].size() ; i++) {
+        int newID = *(*table)[pY-i*ky][pX-i*kx];
+        if(newID != playerID) break;
         counter++;
     }
     return counter;
